c8406585f289c7230c42720fd7f77f6b2e940eab
n3c r0 <moody@localhost.localdomain>
2015-06-24 11:32:04 +0300

    parse HTML, fuck bitches and get money.

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+/*
+ * the famous hello world !
+ * Author: Moody
+ */
+
+#include <stdio.h>
+
+// this function would shoot laser, blow up the moon so the earth would flood 
+// and people would die.
+int main(int argc, char **argv)
+{
+  printf("Hello git!");
+  return 0;
+}
